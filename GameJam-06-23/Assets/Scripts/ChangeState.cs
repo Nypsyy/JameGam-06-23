14cb43b69d82789bf6d34cb7217f78f53c2f3858
Mayeul Marsaut <50460015+Nypsyy@users.noreply.github.com>
2023-06-23 18:44:10 -0400

    Revert "Merge branch 'light'"
    This reverts commit 1f6663331fbd486ea7dae1fa33abc25de5798cdf, reversing
    changes made to 00138ce4f19268583a5c85315e6fac6a34cb2365.

--- GameJam-06-23/Assets/Scripts/ChangeState.cs
+++ GameJam-06-23/Assets/Scripts/ChangeState.cs
@@ -2,13 +2,9 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.U2D.Animation;
-using UnityEngine.Rendering.Universal;
 
 public class ChangeState : MonoBehaviour
 {
-
-    public Light2D Light2D;
-    public SpriteLibrary SpriteLibrary;
 
     [SerializeField]
     private SpriteLibraryAsset[] states;
@@ -25,6 +21,7 @@ public class ChangeState : MonoBehaviour
         
     }
 
+
     public void AlertObservers(string message)
     {
 
@@ -32,7 +29,6 @@ public class ChangeState : MonoBehaviour
             if(i<3){
                 i++;
             }
-            SpriteLibrary.spriteLibraryAsset = states[i];
-            Light2D.intensity-=0.5f;
+            gameObject.GetComponent<SpriteLibrary>().spriteLibraryAsset = states[i];
     }
 }
