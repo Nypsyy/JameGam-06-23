97aaf53eedb0172f7547c8e36dceaff2f7f9b586
Hugo0207 <hgirard002@gmail.com>
2023-06-23 19:35:10 +0200

    LightTestMap

--- GameJam-06-23/Assets/MCmove.cs
+++ GameJam-06-23/Assets/MCmove.cs
@@ -10,7 +10,8 @@ public class MCmove : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+        var rb=GetComponent<Rigidbody2D>();
+        rb.AddForce(new Vector2(10,10),ForceMode2D.Impulse);
     }
 
     // Update is called once per frame
