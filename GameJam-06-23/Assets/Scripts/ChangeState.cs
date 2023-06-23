3148d8e58772546084527c83f65cb616a6672d13
Mayeul Marsaut <50460015+Nypsyy@users.noreply.github.com>
2023-06-23 19:31:32 -0400

    Revert "Merge branch 'main' into light"
    This reverts commit c3077d404811bc4a5dcc332ab8181c182b47d3a9, reversing
    changes made to 6305be9498b8859d473233b9665bb1f43d4042bf.

--- GameJam-06-23/Assets/Scripts/ChangeState.cs
+++ GameJam-06-23/Assets/Scripts/ChangeState.cs
@@ -2,9 +2,13 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.U2D.Animation;
+using UnityEngine.Rendering.Universal;
 
 public class ChangeState : MonoBehaviour
 {
+
+    public Light2D Light2D;
+    public SpriteLibrary SpriteLibrary;
 
     [SerializeField]
     private SpriteLibraryAsset[] states;
@@ -21,14 +25,12 @@ public class ChangeState : MonoBehaviour
         
     }
 
-
     public void AlertObservers(string message)
     {
-
-        
             if(i<3){
                 i++;
             }
-            gameObject.GetComponent<SpriteLibrary>().spriteLibraryAsset = states[i];
+            SpriteLibrary.spriteLibraryAsset = states[i];
+            Light2D.intensity-=0.5f;
     }
 }
